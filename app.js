0dbc654da52218ff4f9cd8aae4814faacea20548
Mizar Bastidas <contasti.mizar@gmail.com>
2021-02-23 14:29:26 -0600

    Cambios hechos

--- app.js
+++ app.js
@@ -1,45 +1,28 @@
+window.addEventListener("dfMessengerLoaded", function (event) {
+  // console.log("chatbot cargado");
 
+  const dfMessenger = document.querySelector("df-messenger");
+  dfMessenger.showMinChat();
 
+  console.log(dfMessenger);
 
+  dfMessenger.renderCustomText("Buenos dìas");
 
-window.addEventListener('dfMessengerLoaded', function (event) {
-    // console.log("chatbot cargado");
+  dfMessenger.addEventListener("df-button-clicked", function (event) {
+    if (event.detail.element.text == "azul") {
+      document.body.style.backgroundColor = "blue";
+    } else if (event.detail.element.text == "rojo") {
+      document.body.style.backgroundColor = "red";
+    } else if (event.detail.element.text == "verde") {
+      document.body.style.backgroundColor = "green";
+    } else if (event.detail.element.text == "amarillo") {
+      document.body.style.backgroundColor = "yellow";
+    } else if (event.detail.element.text == "blanco") {
+      document.body.style.backgroundColor = "white";
+    }
+  });
 
-    const dfMessenger = document.querySelector('df-messenger');
-    dfMessenger.showMinChat();
-
-    console.log(dfMessenger);
-
-    // dfMessenger.renderCustomText('Buenos dìas');
-
-
-
-    dfMessenger.addEventListener('df-button-clicked',function(event){
-
-
-            if(event.detail.element.text == 'azul'){
-  document.body.style.backgroundColor = "blue";
-
-            } else if (event.detail.element.text == 'rojo'){
-
-  document.body.style.backgroundColor = "red";
-
-            } else if (event.detail.element.text == 'verde'){
-
-  document.body.style.backgroundColor = "green";
-
-            } else if(event.detail.element.text == 'amarillo'){
-  document.body.style.backgroundColor = "yellow";
-  } else if(event.detail.element.text == 'blanco'){
-  document.body.style.backgroundColor = "white";
-            }
-
-    });
-
-    dfMessenger.addEventListener('df-user-input-entered',function(event){
-      console.log('df-user-input-entered '+ event.detail.input);
-
-    });
-
-
+  dfMessenger.addEventListener("df-user-input-entered", function (event) {
+    console.log("df-user-input-entered " + event.detail.input);
+  });
 });
